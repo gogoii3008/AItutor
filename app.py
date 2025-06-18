7db6b9635b94c8377b0b250f77130326d87af964
gogoii3008 <siddharthagogoixi@gmail.com>
2025-06-18 14:41:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def bot():
     return str(twilio_response)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # 5000 is fallback
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if PORT is not set
     app.run(host="0.0.0.0", port=port)
